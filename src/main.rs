1f20508186320df06f096462cca9edb8b6ff8c51
riemann <aleqvids@gmail.com>
2024-06-21 19:07:20 -0400

    feat: add cargo-masm-fmt subcommand

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,18 @@ use glob::glob;
 use masm_formatter::format_file;
 
 #[derive(Parser)]
-struct Cli {
+#[command(
+    bin_name = "cargo",
+    subcommand_required = true,
+    arg_required_else_help = true
+)]
+enum Cli {
+    #[command(name = "masm-fmt")]
+    MasmFmt(MasmFmtArgs),
+}
+
+#[derive(Parser)]
+struct MasmFmtArgs {
     /// The pattern to search for (e.g., "**/*.masm")
     pattern: String,
 }
@@ -11,15 +22,19 @@ struct Cli {
 fn main() -> std::io::Result<()> {
     let args = Cli::parse();
 
-    for entry in glob(&args.pattern).expect("Failed to read glob pattern") {
-        match entry {
-            Ok(path) => {
-                if path.extension().and_then(|s| s.to_str()) == Some("masm") {
-                    println!("Formatting file: {:?}", path);
-                    format_file(&path)?;
+    match args {
+        Cli::MasmFmt(args) => {
+            for entry in glob(&args.pattern).expect("Failed to read glob pattern") {
+                match entry {
+                    Ok(path) => {
+                        if path.extension().and_then(|s| s.to_str()) == Some("masm") {
+                            println!("Formatting file: {:?}", path);
+                            format_file(&path)?;
+                        }
+                    }
+                    Err(e) => println!("{:?}", e),
                 }
             }
-            Err(e) => println!("{:?}", e),
         }
     }
 
